1494aa5855b770029f79f1ac44802d9114feae89
Mohamed Yehia <eng.essa85@gmail.com>
2025-06-16 01:02:28 +0300

    try final version of fastapi

--- ai/main.py
+++ ai/main.py
@@ -6,34 +6,36 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.api import router as api_router
 import os
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = int(os.getenv("DB_PORT", 3306))
+# DB_USER = os.getenv("DB_USER")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_NAME = os.getenv("DB_NAME")
 
 
 
-def wait_for_db():
-    for _ in range(10):
-        try:
-            conn = pymysql.connect(
-                host=os.getenv("DB_HOST"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD"),
-                database=os.getenv("DB_NAME"),
-                port=int(os.getenv("DB_PORT")),
-                connect_timeout=5
-            )
-            conn.close()
-            print("✅ DB ready!")
-            return
-        except Exception as e:
-            print(f"⏳ Waiting for DB: {e}")
-            time.sleep(5)
-    raise Exception("❌ DB not ready after retries")
+# def wait_for_db():
+#     for _ in range(10):
+#         try:
+#             conn = pymysql.connect(
+#                 host=os.getenv("DB_HOST"),
+#                 user=os.getenv("DB_USER"),
+#                 password=os.getenv("DB_PASSWORD"),
+#                 database=os.getenv("DB_NAME"),
+#                 port=int(os.getenv("DB_PORT")),
+#                 connect_timeout=5
+#             )
+#             conn.close()
+#             print("✅ DB ready!")
+#             return
+#         except Exception as e:
+#             print(f"⏳ Waiting for DB: {e}")
+#             time.sleep(5)
+#     raise Exception("❌ DB not ready after retries")
 
-wait_for_db()
+# wait_for_db()
+
+
 app = FastAPI()
 
 app.add_middleware(
